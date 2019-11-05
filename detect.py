8dada97fd349a39a73ab548c46acf9295739580b
Filipe <filipe.ftec@gmail.com>
2019-11-05 13:56:33 -0300

    Add multithread to img read and inference using CPU

--- detect.py
+++ detect.py
@@ -9,7 +9,11 @@ import time
 import random
 import imageio
 import imutils
+import threading
+import glob
 
+
+from os import path
 from data import cfg_mnet, cfg_re50
 from layers.functions.prior_box import PriorBox
 from utils.nms.py_cpu_nms import py_cpu_nms
@@ -18,9 +22,15 @@ from utils.box_utils import decode, decode_landm
 from imutils.video import FileVideoStream
 from imutils.video import FPS
 from pathlib import Path
+from multiprocessing import Pool
+from functools import partial
+
+FRAMES_DIR = ".\\tmp\\frames"
+TMP_FRAME_FILE = "{}\\frame{}.jpg"
+TMP_FACE_FILE = '.\\tmp\\face{}.jpg'
+
 
 parser = argparse.ArgumentParser(description='Retinaface')
-
 parser.add_argument('-m', '--trained_model', default='./weights/Resnet50_Final.pth', type=str, help='Trained state_dict file path to open')
 parser.add_argument('--network', default='resnet50', help='Backbone network mobile0.25 or resnet50')
 parser.add_argument('--cpu', action="store_true", default=True, help='Use cpu inference')
@@ -69,128 +79,175 @@ def load_model(model, pretrained_path, load_to_cpu):
     return model
 
 
+def inference(img_raw, device, cfg, net, resize, frame_list):
+    lock = threading.Lock()
+    lock.acquire()
+    
+    print ("Inference over file: {}".format(frame_list))
+    #if len(frame_list) == 0:
+    #    return
+        
+    img_raw = cv2.imread(frame_list ,cv2.IMREAD_COLOR)
+    
+    #frame_list.pop(0)
+    os.remove(frame_list)
+    
+    lock.release()
+    
+    img = np.float32(img_raw)
+    im_height, im_width, _ = img.shape
+    scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+    img -= (104, 117, 123)
+    img = img.transpose(2, 0, 1)
+    img = torch.from_numpy(img).unsqueeze(0)
+    img = img.to(device)
+    scale = scale.to(device)
+    tic = time.time()
+    loc, conf, landms = net(img)  # forward pass
+    print('net forward time: {:.4f}'.format(time.time() - tic))
+
+    priorbox = PriorBox(cfg, image_size=(im_height, im_width))
+    priors = priorbox.forward()
+    priors = priors.to(device)
+    prior_data = priors.data
+    boxes = decode(loc.data.squeeze(0), prior_data, cfg['variance'])
+    boxes = boxes * scale / resize
+    boxes = boxes.cpu().numpy()
+    scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+    landms = decode_landm(landms.data.squeeze(0), prior_data, cfg['variance'])
+    scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                           img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                           img.shape[3], img.shape[2]])
+    scale1 = scale1.to(device)
+    landms = landms * scale1 / resize
+    landms = landms.cpu().numpy()
+
+    # ignore low scores
+    inds = np.where(scores > args.confidence_threshold)[0]
+    boxes = boxes[inds]
+    landms = landms[inds]
+    scores = scores[inds]
+
+    # keep top-K before NMS
+    order = scores.argsort()[::-1][:args.top_k]
+    boxes = boxes[order]
+    landms = landms[order]
+    scores = scores[order]
+
+    # do NMS
+    dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+    keep = py_cpu_nms(dets, args.nms_threshold)
+    # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
+    dets = dets[keep, :]
+    landms = landms[keep]
+
+    # keep top-K faster NMS
+    dets = dets[:args.keep_top_k, :]
+    landms = landms[:args.keep_top_k, :]
+
+    dets = np.concatenate((dets, landms), axis=1)
+
+    # show image
+    if args.save_image:
+        for b in dets:
+            if b[4] < args.vis_thres:
+                continue
+            text = "{:.4f}".format(b[4])
+            b = list(map(int, b))
+            
+            refPoint = [(b[0], b[1]), (b[2], b[3])]
+            
+            cropped = img_raw[refPoint[0][1]:refPoint[1][1], refPoint[0][0]:refPoint[1][0]]
+            name = TMP_FACE_FILE.format(random.randint(1,30000000))
+            cv2.imwrite(name, cropped)
+            
+                        
+            cv2.rectangle(img_raw, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
+            cx = b[0]
+            cy = b[1] + 12
+            cv2.putText(img_raw, text, (cx, cy), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+
+            # landms
+            cv2.circle(img_raw, (b[5], b[6]), 1, (0, 0, 255), 4)
+            cv2.circle(img_raw, (b[7], b[8]), 1, (0, 255, 255), 4)
+            cv2.circle(img_raw, (b[9], b[10]), 1, (255, 0, 255), 4)
+            cv2.circle(img_raw, (b[11], b[12]), 1, (0, 255, 0), 4)
+            cv2.circle(img_raw, (b[13], b[14]), 1, (255, 0, 0), 4)
+
+
 if __name__ == '__main__':
     torch.set_grad_enabled(False)
+    
+    ####x = threading.Thread(target=thread_function, args=(1,))
+    #x.start()
+    #x.join()
+    
+    if not os.path.exists(FRAMES_DIR):
+        os.makedirs(FRAMES_DIR)
+    
     cfg = None
+    cudnn.benchmark = False
+    
+    
     if args.network == "mobile0.25":
         cfg = cfg_mnet
     elif args.network == "resnet50":
         cfg = cfg_re50
+        
     # net and model
     net = RetinaFace(cfg=cfg, phase = 'test')
     net = load_model(net, args.trained_model, args.cpu)
     net.eval()
     print('Finished loading model!')
-    #print(net)
-    cudnn.benchmark = False
+    
     device = torch.device("cpu" if args.cpu else "cuda")
     net = net.to(device)
     resize = 1
 
     #IMAGE LOAD
-    args = {}
-    args["video"] = "C:/Users/Admin/git/Pytorch_Retinaface/curve/trailer.m4v"
+    video_args = {}
+    video_args["video"] = ".\\curve\\trailer.m4v"
         
-    fvs = FileVideoStream(args["video"],queueSize=300).start()
-    time.sleep(1.0)
+    fvs = FileVideoStream(video_args["video"],queue_size=300).start()
+    time.sleep(2.0)
     # loop over frames from the video file stream
     framecount = 0
     fps = FPS().start()
-    print ('before')
-    while fvs.more():
-        print ('after')
+        
+    while fvs.running():
         frame = fvs.read()
-        img_raw = frame
-		#framecount = framecount + 1
-        #print (framecount)
-        #img_raw = None
-		
-        #if framecount%5==0:
-        #    img_raw = frame
-        #else:
-        #    continue
+        #img_raw = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img_raw = None
+
+        framecount = framecount + 1
+        
+        if framecount%10==0:
+            cv2.imwrite(TMP_FRAME_FILE.format(FRAMES_DIR, str(framecount)),frame)
+        else:
+            continue
+            
+        fps.update()
+
+    fps.stop()
+    fvs.stop()
     
-        img = np.float32(img_raw)
-        im_height, im_width, _ = img.shape
-        scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
-        img -= (104, 117, 123)
-        img = img.transpose(2, 0, 1)
-        img = torch.from_numpy(img).unsqueeze(0)
-        img = img.to(device)
-        scale = scale.to(device)
-        tic = time.time()
-        loc, conf, landms = net(img)  # forward pass
-        print('net forward time: {:.4f}'.format(time.time() - tic))
-
-        priorbox = PriorBox(cfg, image_size=(im_height, im_width))
-        priors = priorbox.forward()
-        priors = priors.to(device)
-        prior_data = priors.data
-        boxes = decode(loc.data.squeeze(0), prior_data, cfg['variance'])
-        boxes = boxes * scale / resize
-        boxes = boxes.cpu().numpy()
-        scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
-        landms = decode_landm(landms.data.squeeze(0), prior_data, cfg['variance'])
-        scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
-                               img.shape[3], img.shape[2], img.shape[3], img.shape[2],
-                               img.shape[3], img.shape[2]])
-        scale1 = scale1.to(device)
-        landms = landms * scale1 / resize
-        landms = landms.cpu().numpy()
-
-        # ignore low scores
-        inds = np.where(scores > args.confidence_threshold)[0]
-        boxes = boxes[inds]
-        landms = landms[inds]
-        scores = scores[inds]
-
-        # keep top-K before NMS
-        order = scores.argsort()[::-1][:args.top_k]
-        boxes = boxes[order]
-        landms = landms[order]
-        scores = scores[order]
-
-        # do NMS
-        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
-        keep = py_cpu_nms(dets, args.nms_threshold)
-        # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
-        dets = dets[keep, :]
-        landms = landms[keep]
-
-        # keep top-K faster NMS
-        dets = dets[:args.keep_top_k, :]
-        landms = landms[:args.keep_top_k, :]
-
-        dets = np.concatenate((dets, landms), axis=1)
-
-        # show image
-        if args.save_image:
-            for b in dets:
-                if b[4] < args.vis_thres:
-                    continue
-                text = "{:.4f}".format(b[4])
-                b = list(map(int, b))
-                
-                refPoint = [(b[0], b[1]), (b[2], b[3])]
-                
-                cropped = img_raw[refPoint[0][1]:refPoint[1][1], refPoint[0][0]:refPoint[1][0]]
-                name = "tmp//face{}.jpg".format(random.randint(1,30000000))
-                cv2.imwrite(name, cropped)
-                
-                            
-                cv2.rectangle(img_raw, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
-                cx = b[0]
-                cy = b[1] + 12
-                cv2.putText(img_raw, text, (cx, cy), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
-
-                # landms
-                cv2.circle(img_raw, (b[5], b[6]), 1, (0, 0, 255), 4)
-                cv2.circle(img_raw, (b[7], b[8]), 1, (0, 255, 255), 4)
-                cv2.circle(img_raw, (b[9], b[10]), 1, (255, 0, 255), 4)
-                cv2.circle(img_raw, (b[11], b[12]), 1, (0, 255, 0), 4)
-                cv2.circle(img_raw, (b[13], b[14]), 1, (255, 0, 0), 4)
-            # save image
-
-            #name = "test.jpg"
-            #cv2.imwrite(name, img_raw)
-
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    
+    
+    frame_list = [f for f in glob.glob("{}\\*.jpg".format(FRAMES_DIR))]
+    func = partial(inference, img_raw, device, cfg,  net, resize)
+    
+    p = Pool(3)
+    p.map(func, frame_list)
+    
+    #x = threading.Thread(target=inference, args=(img_raw, net, resize))
+    #x.start()
+    #x.join()
+    
+    #inference(img_raw, net, resize)
+    
+    
+    
+    
+    
